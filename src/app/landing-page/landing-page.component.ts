f22b56efe92c89ec036ec676f50b1ac15aebff1c
Kritika Singla <kritikasingla17@gmail.com>
2020-05-07 05:21:38 +0530

    integraate lead table with api

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import test_Data from '../../assets/data.js';
+import {LandingPageService} from './landing-page.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,10 +9,20 @@ import test_Data from '../../assets/data.js';
 })
 export class LandingPageComponent implements OnInit {
   public lead_data;
-  constructor() { }
+  constructor(private LandingPageService:LandingPageService) { }
 
   ngOnInit() {
+    this.getLeadData();
     this.lead_data=test_Data;
+    // console.log(this.lead_data)
+  }
+
+  getLeadData=()=>{
+    this.LandingPageService.getLeadList().subscribe(
+      data=>{this.lead_data=data; },
+      err=>{console.log(err)},
+      ()=>{console.log('done loading lead details')}
+    )
   }
 
 }
---
+++ src/app/landing-page/landing-page.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import {Observable} from 'rxjs/Observable';
+@Injectable()
+export class LandingPageService {
+
+  constructor(private http:HttpClient) { }
+
+  getLeadList=()=>{
+    return this.http.get('/api/leads/?location_string=India');
+  }
+
+}
