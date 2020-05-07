38ffe7a558f04685d1ffe7c0b2e1d8d6fd39216a
Kritika Singla <kritikasingla17@gmail.com>
2020-05-07 22:24:25 +0530

    bug fix

--- src/app/landing-page/landing-page.service.ts
+++ src/app/landing-page/landing-page.service.ts
@@ -8,20 +8,20 @@ export class LandingPageService {
   constructor(private http:HttpClient) { }
 
   getLeadList=()=>{
-    return this.http.get(`http://3.219.31.158:4059/api/leads/?location_string=India`);
+    return this.http.get(`http://3.228.13.3:4059/api/leads/?location_string=India`);
   }
 
   addNewLead=(lead)=>{
     console.log(lead)
-    return this.http.post("http://3.219.31.158:4059/api/leads/",lead)
+    return this.http.post("http://3.228.13.3:4059/api/leads/",lead)
   }
 
   deleteLead=(id)=>{
-    return this.http.delete(`http://3.219.31.158:4059/api/leads/${id}/`);
+    return this.http.delete(`http://3.228.13.3:4059/api/leads/${id}/`);
   }
 
   updateLead=(lead)=>{
-    return this.http.put(`http://3.219.31.158:4059/api/mark_lead/${lead.id}`,lead);
+    return this.http.put(`http://3.228.13.3:4059/api/mark_lead/${lead.id}`,lead);
   }
 
 }
