f48846622e276d40806711c74d56a88db2bd1546
Kritika Singla <kritikasingla17@gmail.com>
2020-05-07 22:30:51 +0530

    api url fix

--- src/app/landing-page/landing-page.service.ts
+++ src/app/landing-page/landing-page.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import environment from '../../environments/environment';
-// import {Observable} from 'rxjs/Observable';
+import {environment} from '../../environments/environment'
 @Injectable()
 export class LandingPageService {
 
+  private apiUrl=environment.apiUrl;
   constructor(private http:HttpClient) { }
 
   getLeadList=()=>{
-    return this.http.get(`http://3.219.31.158:4059/api/leads/?location_string=India`);
+    return this.http.get(`${this.apiUrl}/api/leads/?location_string=India`);
   }
 
   addNewLead=(lead)=>{
     console.log(lead)
-    return this.http.post("http://3.219.31.158:4059/api/leads/",lead)
+    return this.http.post(`${this.apiUrl}/api/leads/`,lead)
   }
 
   deleteLead=(id)=>{
-    return this.http.delete(`http://3.219.31.158:4059/api/leads/${id}/`);
+    return this.http.delete(`${this.apiUrl}/api/leads/${id}/`);
   }
 
   updateLead=(lead)=>{
-    return this.http.put(`http://3.219.31.158:4059/api/mark_lead/${lead.id}`,lead);
+    return this.http.put(`${this.apiUrl}/api/mark_lead/${lead.id}`,lead);
   }
 
 }
